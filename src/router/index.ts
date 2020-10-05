cfcdb85fafb075d2f86af2867b86cb90371bb562
Asam <xiangmaoxianxg@gmail.com>
2020-10-05 20:22:02 +0800

    feat: 解决重写push方法时，无法正确定位类型

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,18 @@
 import Vue from 'vue'
-import VueRouter, { RawLocation } from 'vue-router'
+import VueRouter, { RawLocation, Route } from 'vue-router'
 import { constantRoutes } from './routes'
 
 Vue.use(VueRouter)
 
+// 解决使用call无法定位正确的类型，由于原声明文件中使用了函数重载，所以重写VueRouter类型
+interface MyRouter extends VueRouter {
+  push(location: RawLocation): Promise<Route>
+}
+
 // 解决重复引用同一个路由的问题，重写push
-const _push = VueRouter.prototype.push
+const _push = (VueRouter.prototype as MyRouter).push
 VueRouter.prototype.push = function(location: RawLocation) {
-  return (_push.call(this, location) as any).catch((err: Error) => err)
+  return _push.call(this, location).catch(err => err)
 }
 
 const router = new VueRouter({
