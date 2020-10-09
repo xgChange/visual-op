4c786755b1bc83f46fb974ae488f717af96d2d54
xiangmaoxian <xiangmaoxian@unittec.com>
2020-10-09 15:02:13 +0800

    feat: 增加vue.config.js的一些基础配置

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,16 @@
+const isProd = process.env.NODE_ENV === 'production'
+
 module.exports = {
+  publicPath: isProd ? './' : '/',
+  productionSourceMap: !isProd,
   css: {
     loaderOptions: {
       sass: {
         prependData: `@import "@/assets/styles/_variables.scss";`
       }
     }
+  },
+  devServer: {
+    port: 8765
   }
 }
