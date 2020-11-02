2d08f7db0c1ac300d3fa0a12f7282fa748e0458a
xiangmaoxian <xiangmaoxian@unittec.com>
2020-11-02 14:41:41 +0800

    feat: 添加多页面配置，devserver下通过/subpage.html访问

---
+++ src/subpage/main.ts
@@ -0,0 +1,5 @@
+/**
+ * @description 子页面的入口
+ */
+
+console.log('子页面')
--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,31 @@
 const isProd = process.env.NODE_ENV === 'production'
 
+const pages = {
+  main: {
+    entry: 'src/main.ts',
+    template: 'public/index.html',
+    filename: 'index.html',
+    title: '管理系统'
+  },
+  subpage: {
+    entry: 'src/subpage/main.ts',
+    template: 'public/index.html',
+    filename: 'subpage.html'
+  }
+}
+
 module.exports = {
   publicPath: isProd ? './' : '/',
   productionSourceMap: !isProd,
+  pages, // 配置多页面
+  chainWebpack: config => {
+    if (pages) {
+      Object.keys(pages).forEach(page => {
+        config.plugins.delete(`preload-${page}`)
+        config.plugins.delete(`prefetch-${page}`)
+      })
+    }
+  },
   css: {
     loaderOptions: {
       sass: {
