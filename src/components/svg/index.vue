d5b9282aed1d540b95104e068ab7f55336f7ba75
xiangmaoxian <xiangmaoxian@unittec.com>
2020-11-05 17:49:44 +0800

    feat: 添加svg基础配置

---
+++ src/components/svg/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <svg class="icon" aria-hidden="true">
+    <use :xlink:href="iconName"></use>
+  </svg>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'vue-property-decorator'
+
+@Component
+export default class MySvg extends Vue {
+  @Prop({
+    type: String,
+    default: ''
+  })
+  private iconClass!: string
+
+  set iconName(val: string) {
+    this.iconName = `#icon-${this.iconClass}`
+  }
+
+  get iconName() {
+    return this.iconName
+  }
+}
+</script>
+
+<style></style>
--- src/mock/data/visual.ts
+++ src/mock/data/visual.ts
@@ -2,4 +2,15 @@
  * @description 关于可视化编辑界面的一些假数据
  */
 
-export const utilsData = [{}]
+export const utilsData = [
+  {
+    id: 10001,
+    title: '常用',
+    content: [
+      {
+        id: 1001,
+        iconName: ''
+      }
+    ]
+  }
+]
